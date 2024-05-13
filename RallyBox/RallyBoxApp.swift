26f8568ed889cceb551f89ff1317f63ba7e0e171
Muhan Li <mlfromchina@gmail.com>
2024-05-13 14:39:49 -0700

    Initial Commit

---
+++ RallyBox/RallyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyBoxApp.swift
+//  RallyBox
+//
+//  Created by Muhan Li on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct RallyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
